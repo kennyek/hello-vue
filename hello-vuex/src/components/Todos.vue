b24224074d1f30e9d3c3b58f467def66a16350ff
Kenny Ek <ekkenny@hotmail.com>
2019-04-07 21:57:04 +0200

    Add delete, filter and update
    According to tutorial.

--- hello-vuex/src/components/Todos.vue
+++ hello-vuex/src/components/Todos.vue
@@ -1,9 +1,25 @@
 <template>
   <div>
     <h3>Todos</h3>
+    <div class="legend">
+      Double click to mark as complete
+      <span>
+        <span class="incomplete-box"></span> = Incomplete
+      </span>
+      <span>
+        <span class="complete-box"></span> = Complete
+      </span>
+    </div>
     <div class="todos">
-      <div v-for="todo in allTodos" :key="todo.id" class="todo">
+      <div
+        @dblclick="onDoubleClick(todo)"
+        v-for="todo in allTodos"
+        :key="todo.id"
+        class="todo"
+        :class="{'is-complete':todo.completed}"
+      >
         {{ todo.title }}
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
       </div>
     </div>
   </div>
@@ -15,7 +31,16 @@ import { mapGetters, mapActions } from 'vuex';
 export default {
   name: 'Todos',
   methods: {
-    ...mapActions(['fetchTodos']),
+    ...mapActions(['fetchTodos', 'deleteTodo', 'updateTodo']),
+    onDoubleClick(todo) {
+      const updatedTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed,
+      };
+
+      this.updateTodo(updatedTodo);
+    },
   },
   computed: mapGetters(['allTodos']),
   created() {
@@ -25,19 +50,58 @@ export default {
 </script>
 
 <style scoped>
-  .todos {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 1rem;
-  }
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
 
-  .todo {
-    border: 1px solid #ccc;
-    background: #41b883;
-    padding: 1rem;
-    border-radius: 5px;
-    text-align: center;
-    position: relative;
-    cursor: pointer;
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
+
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
   }
+}
 </style>
--- hello-vuex/src/store/modules/todos.js
+++ hello-vuex/src/store/modules/todos.js
@@ -13,6 +13,7 @@ const actions = {
     const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
     commit('setTodos', response.data);
   },
+
   async addTodo({ commit }, title) {
     const response = await axios.post(
       'https://jsonplaceholder.typicode.com/todos',
@@ -24,6 +25,30 @@ const actions = {
 
     commit('newTodo', response.data);
   },
+
+  async deleteTodo({ commit }, id) {
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`);
+
+    commit('removeTodo', id);
+  },
+
+  async filterTodos({ commit }, event) {
+    const selectValue = event.target.value;
+    const limit = Number(selectValue);
+
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`);
+
+    commit('setTodos', response.data);
+  },
+
+  async updateTodo({ commit }, updatedTodo) {
+    const response = await axios.put(
+      `https://jsonplaceholder.typicode.com/todos/${updatedTodo.id}`,
+      updatedTodo,
+    );
+
+    commit('updateTodo', response.data);
+  },
 };
 
 const mutations = {
@@ -32,6 +57,19 @@ const mutations = {
   },
   newTodo: (passedState, todo) => {
     passedState.todos.unshift(todo);
+  },
+  removeTodo: (passedState, id) => {
+    passedState.todos = passedState.todos.filter(todo => todo.id !== id);
+  },
+  filterTodos: (passedState, todos) => {
+    passedState.todos = todos;
+  },
+  updateTodo: (passedState, updatedTodo) => {
+    const index = passedState.todos.findIndex(todo => todo.id === updatedTodo.id);
+
+    if (index !== -1) {
+      passedState.todos.splice(index, 1, updatedTodo);
+    }
   },
 };
 
